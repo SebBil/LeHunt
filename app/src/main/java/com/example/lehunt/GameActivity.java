38ed5caa2f0d4fc325b125a6b57d366e222f7a17
SebBil <sebastian.bilda@outlook.de>
2020-01-05 17:21:49 +0100

    Coderefactoring, remove BLEScanner, implement jaalee beacon manager

--- app/src/main/java/com/example/lehunt/GameActivity.java
+++ app/src/main/java/com/example/lehunt/GameActivity.java
@@ -4,16 +4,22 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
-import android.annotation.TargetApi;
 import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.le.ScanCallback;
-import android.bluetooth.le.ScanResult;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.RemoteException;
 import android.util.Log;
-import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.jaalee.sdk.Beacon;
+import com.jaalee.sdk.BeaconManager;
+import com.jaalee.sdk.RangingListener;
+import com.jaalee.sdk.Region;
+import com.jaalee.sdk.ServiceReadyCallback;
 
 import org.eclipse.paho.android.service.MqttAndroidClient;
 import org.eclipse.paho.client.mqttv3.DisconnectedBufferOptions;
@@ -25,18 +31,25 @@ import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
-import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GameActivity extends AppCompatActivity {
+
+    private static final String JAALEE_BEACON_PROXIMITY_UUID = "EBEFD083-70A2-47C8-9837-E7B5634DF524";//Jaalee BEACON Default UUID
+    private static final Region ALL_BEACONS_REGION = new Region("rid", null, null, null);
 
     private static final int PERMISSION_REQUEST_COARSE_LOCATION = 456;
     private String TAG = "GameActivity";
 
     private MqttAndroidClient mqttClient;
-    private BLEScanner bleScanner;
+    private BeaconManager beaconManager;
 
     private String topicPub, topicSub;
     private int REQUEST_ENABLE_BT = 1;
+    private Beacon beacon;
+    private boolean PubMessageSend = false;
+    private LinearLayout hints;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,18 +64,68 @@ public class GameActivity extends AppCompatActivity {
         topicPub = curHunt.getHuntID() + "/" + curHunt.getClientID() + "/up";
         topicSub = curHunt.getHuntID() + "/" + curHunt.getClientID() + "/down";
 
-        // TODO: 27.12.2019 Start thread/service for checking all devices in the environment maybe BCReceiver is enough
+        hints = findViewById(R.id.ll_hints);
 
-        // TODO: 27.12.2019 subscribe the topic of the cliendid
+         // Configure BeaconManager.
+         beaconManager = new BeaconManager(this);
+         beaconManager.setRangingListener(new RangingListener() {
+            @Override
+            public void onBeaconsDiscovered(Region region, final List beacons) {
+                // Note that results are not delivered on UI thread.
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
 
-        /** Check Bluetooth is enabled and the adapter is not null, Start it if one is true */
-         if(!BluetoothAdapter.getDefaultAdapter().isEnabled()){
-             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-         }
+                        // Note that beacons reported here are already sorted by estimated
+                        // distance between device and beacon.
 
-         bleScanner = new BLEScanner();
-         bleScanner.beginScanning(mqttClient);
+                        if (beacons.size() > 0){
+                            beacon = (Beacon) beacons.get(0);
+                            if ( beacon.getProximityUUID().equalsIgnoreCase(JAALEE_BEACON_PROXIMITY_UUID) ) {
+                                if (beacon.getRssi() > -50) {
+                                    // Log.i("JAALEE", "JAALEE:" + beacon.getBattLevel());
+                                    if (!PubMessageSend) {
+                                        try {
+                                            mqttClient.publish(topicPub, new MqttMessage("new Hint".getBytes()));
+                                            LinearLayout.LayoutParams lparams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                                            TextView tv = new TextView(getApplicationContext());
+                                            tv.setLayoutParams(lparams);
+                                            tv.setText("Message to mqtt send and new Hint is comming soon");
+                                            hints.addView(tv);
+                                        } catch (MqttException e) {
+                                            e.printStackTrace();
+                                        }
+                                    }
+                                    PubMessageSend = true;
+                                }
+                                Log.d("BEACON", "Name: " + beacon.getName());
+                                Log.d("BEACON", "UUID: " + beacon.getProximityUUID());
+                                Log.d("BEACON", "Major: " + beacon.getMajor());
+                                Log.d("BEACON", "Minor: " + beacon.getMinor());
+                            }
+                            //Log.d("BEACON", beacon.getName() + "");
+                        }
+                        //List<Beacon> JaaleeBeacons = filterBeacons(beacons);
+
+
+                    }
+                });
+            }
+         });
+
+         /*BLE device around the phone
+         beaconManager.setDeviceDiscoverListener(new DeviceDiscoverListener() {
+            @Override
+            public void onBLEDeviceDiscovered(BLEDevice device) {
+                // TODO Auto-generated method stub
+                Log.i("JAALEE", "On ble device  discovery:" + device.getMacAddress());
+                Log.i("JAALEE", device.toString());
+
+            }
+         });*/
+
+         //bleScanner = new BLEScanner();
+         //bleScanner.beginScanning(mqttClient);
 
          mqttClient = new MqttAndroidClient(getApplicationContext(), curHunt.getBrokerURL(), curHunt.getClientID());
          mqttClient.setCallback(new MqttCallbackExtended() {
@@ -121,6 +184,53 @@ public class GameActivity extends AppCompatActivity {
 
     }
 
+     @Override
+     protected void onStart(){
+        super.onStart();
+
+         // Check if device supports Bluetooth Low Energy.
+         if (!beaconManager.hasBluetooth()) {
+             Toast.makeText(this, "Device does not have Bluetooth Low Energy", Toast.LENGTH_LONG).show();
+             return;
+         }
+
+         // If Bluetooth is not enabled, let user enable it.
+         if (!beaconManager.isBluetoothEnabled()) {
+             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+         } else {
+             connectToService();
+         }
+     }
+
+    private void connectToService() {
+        beaconManager.connect(new ServiceReadyCallback() {
+            @Override
+            public void onServiceReady() {
+                try {
+                    beaconManager.startRangingAndDiscoverDevice(ALL_BEACONS_REGION);
+                } catch (RemoteException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+    private List<Beacon> filterBeacons(List<Beacon> beacons) {
+        List<Beacon> filteredBeacons = new ArrayList<Beacon>(beacons.size());
+        for (Beacon beacon : beacons)
+        {
+//    	only detect the Beacon of Jaalee
+    	if ( beacon.getProximityUUID().equalsIgnoreCase(JAALEE_BEACON_PROXIMITY_UUID) )
+    	if (beacon.getRssi() > -50)
+            {
+                Log.i("JAALEE", "JAALEE:"+beacon.getBattLevel());
+                filteredBeacons.add(beacon);
+            }
+        }
+        return filteredBeacons;
+    }
+
     private void subscribeToTopic(){
         try{
             mqttClient.subscribe(topicSub, 0, null, new IMqttActionListener() {
@@ -162,7 +272,6 @@ public class GameActivity extends AppCompatActivity {
     private void pubishMsgForNewHints(String beaconID, int qos){
         try {
             Log.d(TAG, "publish beacon ID to topic for new Hints");
-            //mqttClient.subscribe()
             //huntMqttClient.publishMessage(mqttClient, beaconID, qos, topicPub);
         } catch (Exception e){
             e.printStackTrace();
@@ -189,9 +298,18 @@ public class GameActivity extends AppCompatActivity {
 
     @Override
     protected void onDestroy(){
-        super.onDestroy();
         Log.d(TAG, "onDestroy");
+
+        Log.d("BM", "BeaconManager disconnected");
+        beaconManager.disconnect();
         Log.d(TAG, "clear MQTT Service");
+        try {
+            mqttClient.disconnect();
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+
+        super.onDestroy();
         // TODO: 03.01.2020 clear MQTT Service and Broadcast Receiver for BLE
         //stopService(mqttService);
     }
